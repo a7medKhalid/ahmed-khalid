0543698e4a8b90eb8821906fecc22fea7f923b6a
ahmed <ahmed1alghamdi2000@gmail.com>
2023-09-17 23:52:35 +0300

    updated

--- src/component/ServicesPage.js
+++ src/component/ServicesPage.js
@@ -8,21 +8,22 @@ class ServicesPage extends Component {
             <section>
             <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
                 <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
-                    <h2 className="mb-4 text-3xl font-extrabold tracking-tight  text-white"> لديك فكرة مشروع أو تود تطوير مشروعك الحالي؟</h2>
-                    <section className="mb-5 font-light  sm:text-xl text-gray-400">
-                        <p>🎉 لقد وصلت </p>
-                        <p> سيتم البدأ معك من فهم المشكلة التي تعانيها الفئة المستهدفة لننطلق منها بحلول تقنية تسعد عملائك وتجلب الربح </p>
+                    <h2 className="mb-4 text-xl font-extrabold tracking-tight  text-white">لديك فكرة مشروع تقني أو ترغب في تطوير مشروعك الحالي؟</h2>
+                    <section className="mb-5 font-light  text-gray-400 w-full">
+                        <p className={"text-xl"}>🎉 لقد وصلت </p>
+                        <p className={"md:text-sm"}>  البداية ستكون باستشارة لتحليل فكرة المشروع وتحديد أنسب الحلول التقنية وخطة التطوير </p>
                     </section>
 
-                    <section className="mb-5 font-light  sm:text-xl text-gray-400">
-                        <h2>📜 نعدك بثلاث وعود  </h2>
-                        <ul>
-                            <li>١-الوصول للعملاء بأسرع وقت لأننا نستخدم منهجية الإدارة الرشيقة</li>
-                            <li>٢- الالتزام بمعايير جودة البرمجيات لتجنب مشاكل الاطلاق وتسهيل التعديلات لاحقًا</li>
-                            <li>٣- تجربة مستخدم ممتعة لأننا سننطلق مما يحتاجه المستخدم</li>
-                        </ul>
+                    <section className="mb-5 ">
+                        <h2 className=" font-extrabold tracking-tight text-white">  التزاماتنا</h2>
+                        <ol className="font-light md:text-sm text-gray-400 ">
+                            <li className={"after:content-['-']"}>الوصول للعملاء بأسرع وقت لأننا نستخدم منهجية  الإدارة الرشيقة </li>
+                            <li className={"after:content-['-']"}>الالتزام بمعايير جودة البرمجيات لتجنب مشاكل الاطلاق وتسهيل التعديلات لاحقًا</li>
+                            <li className={"after:content-['-']"}>تجربة مستخدم ممتعة لأننا سننطلق مما يحتاجه المستخدم</li>
+                            <li className={"after:content-['-']"}>التزام بالمواعيد والتسليم في الوقت المحدد</li>
+                        </ol>
                     </section>
-                    <a href="https://wa.me/message/VENQWFYQPBWEN1" className=" bg-green-600 hover:bg-green-700 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white  focus:ring-green-900"> ابدأ الآن</a>
+                    <a href="https://wa.me/message/VENQWFYQPBWEN1" className=" bg-green-600 hover:bg-green-700 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white  focus:ring-green-900"> ابدأ باستشارة مجانية</a>
                     <br/>
                     <br/>
                     <br/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,21 +28,17 @@ const IndexPage = () => {
     <div className="w-full min-h-screen" style={{backgroundColor:"#1a202c"}}>
 
 
-        <div className=" md:fixed md:top-0 md:left-0 w-full text-white p-8 " style={{backgroundColor:"#1a202c"}}>
+        <div className=" md:fixed md:top-0 md:left-0 w-full text-white p-8 md:p-0 " style={{backgroundColor:"#1a202c"}}>
            <div className="flex items-center justify-center flex-col md:flex-row-reverse">
                 <div>
-                    <img className="md:ml-4 mt-4 h-36 w-36 rounded-full ring-2 ring-white" src={Avatar} alt=""/>
+                    <img className="md:ml-4 mt-4 md:h-14 md:w-14 h-36 w-36 rounded-full ring-2 ring-white" src={Avatar} alt=""/>
                 </div>
                 <div className="mt-8 md:mr-8">
                     <div className="p-4">
-                        <h1 className="text-4xl font-bold">أحمد خالد</h1>
-                        <p className="text-2xl"> مهندس برمجيات</p>
+                        <h1 className="text-4xl md:text-xl font-bold">أحمد خالد</h1>
+                        <p className="text-xs text-gray-400 "> مهندس برمجيات عملت مع العديد من الجهات والأفراد <br/> على تطوير مشاريعهم التقنية</p>
                     </div>
-                    <Twitter className="inline-block w-8 h-8 mr-2" />
-                    <Youtube className="inline-block w-10 h-10 mr-2" />
-                    <Whatsapp className="inline-block w-8 h-8 mr-2 " />
-                    <Github className="inline-block w-8 h-8 mr-2" />
-                    <Linkedin className="inline-block w-8 h-8 mr-2" />
+
                    
                 
                 </div>
@@ -51,22 +47,22 @@ const IndexPage = () => {
 
         <div className={`bg-slate-900 flex md:flex-col px-4 md:px-0  fixed translate-x-1/2 md:translate-x-1 right-1/2 md:right-0 bottom-0 md:bottom-1/2 md:translate-y-1/2 rounded-2xl md:rounded-r md:rounded-l-2xl mb-8 md:mb-0 text-white`}>
 
-            <div className="relative flex flex-col items-center p-2 md:p-4">
+            <div className="relative flex flex-col items-center p-2 md:p-2">
                 <div className={`absolute top-0 md:left-0 left-1/4 md:top-1/4 w-1/2 md:h-1/2 h-1 md:w-1 ${selectedComponent === 'articles' ? 'bg-green-500' : 'bg-gray-300'}`}></div>
-                <Articles className="inline-block w-8 h-8 md:w-12 md:h-12 mr-1 md:mr-2 hover:animate-bounce" onClick={() => setSelectedComponent('articles')}/>
+                <Articles className="inline-block w-8 h-8 md:w-8 md:h-8 mr-1 md:mr-2 hover:animate-bounce" onClick={() => setSelectedComponent('articles')}/>
                 <p className="text-xs mt-1 md:text-sm"> مقالات</p>
             </div>
 
-            <div className="relative flex flex-col items-center p-2 md:p-4">
+            <div className="relative flex flex-col items-center p-2 md:p-2">
                 <div className={`absolute top-0 md:left-0 left-1/4 md:top-1/4 w-1/2 md:h-1/2 h-1 md:w-1 ${selectedComponent === 'projects' ? 'bg-green-500' : 'bg-gray-300'}`}></div>
-                <Projects className="inline-block w-8 h-8 md:w-12 md:h-12 mr-1 md:mr-2 hover:animate-bounce " onClick={() => setSelectedComponent('projects')}/>
+                <Projects className="inline-block w-8 h-8 md:w-8 md:h-8 mr-1 md:mr-2 hover:animate-bounce " onClick={() => setSelectedComponent('projects')}/>
                 <p className="text-xs mt-1 md:text-sm"> المشاريع</p>
             </div>
 
-            <div className="relative flex flex-col items-center p-2 md:p-4">
+            <div className="relative flex flex-col items-center p-2 md:p-2">
                 <div className={`absolute top-0 md:left-0 left-1/4 md:top-1/4 w-1/2 md:h-1/2 h-1 md:w-1 ${selectedComponent === 'services' ? 'bg-green-500' : 'bg-gray-300'}`}></div>
-                <Services className="inline-block w-8 h-8 md:w-12 md:h-12 mr-1 md:mr-2 hover:animate-bounce" onClick={() => setSelectedComponent('services')}/>
-                <p className="text-xs mt-1 md:text-sm"> الخدمات</p>
+                <Services className="inline-block w-8 h-8 md:w-8 md:h-8 mr-1 md:mr-2 hover:animate-bounce" onClick={() => setSelectedComponent('services')}/>
+                <p className="text-xs mt-1 md:text-sm"> الرئيسة</p>
             </div>
          
 
@@ -75,10 +71,19 @@ const IndexPage = () => {
         </div>
 
 
-        <div className="content-area md:mt-56 ">
+        <div className="content-area md:mt-14 ">
+            <div className="flex justify-center items-center">
+                <Twitter className="inline-block w-8 h-8 mr-2" />
+                <Youtube className="inline-block w-10 h-10 mr-2" />
+                <Whatsapp className="inline-block w-8 h-8 mr-2 " />
+                <Github className="inline-block w-8 h-8 mr-2" />
+                <Linkedin className="inline-block w-8 h-8 mr-2" />
+            </div>
         {selectedComponent === 'projects' && <ProjectsPage />}
         {selectedComponent === 'services' && <ServicesPage />}
             {selectedComponent === 'articles' && <ArticlesPage />}
+
+
       </div>
 
 
